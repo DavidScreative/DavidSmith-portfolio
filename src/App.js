4d826682fe62408d74da66701001d30dfa6c6dbe
David Smith <creepingcats@btconnect.com>
2018-08-09 15:28:10 +0100

    Updated

--- src/App.js
+++ src/App.js
@@ -11,12 +11,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">David Smith Developer Portfolio - IN DEVELOPMENT COMING SEPTEMBER 2018!</h1>
         </header>
-        <p className="App-intro">
-          Welcome to my personal developer portfolio which is currently in development.
-          I am currently at the Kodiri bootcamp and learning Javascript and React from scratch.
-          Make sure you follow my progress on twitter @DavidScreative and my linkedIn profile.  I am looking for an internship/junior dev position and enjoy developing for the web and voice services such as Alexa and Google Assistant.
-          I am active at technology meetups and regularly attend Amazon Appsters, Alexa Devs, Linuxing in London (I am co-organiser of this group), and enjoy attending hackathons and voice development workshops.</p>
-      <img src="./images/computerkeyboard.jpg>" className="Image" alt="Image of a laptop"/>
+        <div className="App-intro">
+          <p>Welcome to my personal developer portfolio which is currently in development.</p>
+          <p>I am currently at the Kodiri bootcamp and learning Javascript and React from scratch.</p>
+          <p>Make sure you follow my progress on twitter @DavidScreative and my linkedIn profile.  I am looking for an internship/junior dev position and enjoy the challenge of doing things outside my comfort zone and also learning to develop for voice apps such as Alexa and Google Assistant.</p>
+          <p>I am active at technology meetups and regularly attend Amazon Appsters, Alexa Devs, Linuxing in London (I am co-organiser of this group), and enjoy attending hackathons and voice development workshops.</p>
+          <img src="./images/computerkeyboard.jpg>" className="Image" alt="Image of a laptop"/>
+      </div>
       </div>
     );
   }
